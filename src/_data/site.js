ee942cf3bcd3ab8ca511000f062512d2711dcdb6
MenilV <menil.vukovic@gmail.com>
2026-02-28 14:38:11 +0100

    SEO Improvements

---
+++ src/_data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "Menil Vuković",
+    url: "https://menilvukovic.com",
+    description: "Engineer, CEO, Writer. Building digital products at Pear Shadow.",
+    author: {
+        name: "Menil Vuković",
+        email: "hello@menilvukovic.com",
+        twitter: "@MenilVukovic"
+    }
+};
